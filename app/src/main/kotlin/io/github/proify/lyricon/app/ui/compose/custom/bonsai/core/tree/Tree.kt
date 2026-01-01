b3d2e40faa5fbdf0451bc790dd5d0c76e7341275
Proify <247336249+proify@users.noreply.github.com>
2026-01-01 16:43:48 +0800

    first

--- app/src/main/kotlin/io/github/proify/lyricon/app/ui/compose/custom/bonsai/core/tree/Tree.kt
+++ app/src/main/kotlin/io/github/proify/lyricon/app/ui/compose/custom/bonsai/core/tree/Tree.kt
@@ -1,5 +1,24 @@
+/*
+ * Lyricon – An Xposed module that extends system functionality
+ * Copyright (C) 2026 Proify
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.proify.lyricon.app.ui.compose.custom.bonsai.core.tree
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Composition
 import androidx.compose.runtime.Immutable
@@ -30,6 +49,7 @@ class Tree<T> internal constructor(
 ) : ExpandableTree<T> by ExpandableTreeHandler(nodes),
     SelectableTree<T> by SelectableTreeHandler(nodes)
 
+@SuppressLint("ComposableNaming")
 @Composable
 fun <T> Tree(
     content: @Composable TreeScope.() -> Unit
--- app/src/main/kotlin/io/github/proify/lyricon/app/ui/preference/SwitchPreference.kt
+++ app/src/main/kotlin/io/github/proify/lyricon/app/ui/preference/SwitchPreference.kt
@@ -1,5 +1,24 @@
+/*
+ * Lyricon – An Xposed module that extends system functionality
+ * Copyright (C) 2026 Proify
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.proify.lyricon.app.ui.preference
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
@@ -25,7 +44,7 @@ fun SwitchPreference(
     leftAction: @Composable (() -> Unit)? = null,
     rightActions: @Composable RowScope.() -> Unit = {},
     switchColors: SwitchColors = SwitchDefaults.switchColors(),
-    modifier: Modifier = Modifier,
+    @SuppressLint("ModifierParameter") modifier: Modifier = Modifier,
     insideMargin: PaddingValues = BasicComponentDefaults.InsideMargin,
     onClick: (() -> Unit)? = null,
     holdDownState: Boolean = false,
